d02f4a7f043c2b60023dd2cbf53ffb7b374987fe
Gabriel Silva <gabrielsos99@gmail.com>
2020-08-06 00:31:36 -0300

    frontend finished

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,36 +3,53 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+export interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
     <header>
       <img
-        src="https://avatars3.githubusercontent.com/u/50718745?s=460&u=0df825418051b9ee9f88a0ee04d9f1dc622eab87&v=4"
-        alt="Gabriel Silva"
+        src={teacher.avatar}
+        alt={teacher.name}
       />
       <div>
-        <strong>Gabriel Silva</strong>
-        <span>Química</span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
       </div>
     </header>
 
-    <p>
-      Entusiasta das melhores tecnologias de química avançada.
-      <br /><br />
-      Apaixonado por explodir coisas em laboratório e por mudar a vidas das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-    </p>
+    <p>{teacher.bio}</p>
 
     <footer>
       <p>
         Preço/hora
-        <strong>R$ 80,00</strong>
+        <strong>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(teacher.cost)}</strong>
       </p>
-      <button type="button">
+      <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
         <img src={whatsappIcon} alt="Whatsapp"/>
         Entrar em contato
-      </button>
+      </a>
     </footer>
   </article>
   );
